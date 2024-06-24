a1d81a088940fc9fad149137c7cb459d8dcdb64f
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2024-06-24 02:03:21 -0300

    update

--- src/components/post.form.tsx
+++ src/components/post.form.tsx
@@ -7,7 +7,16 @@ import TagLoader from './tag.loader'
 import axios from 'axios'
 import toast from 'react-hot-toast'
 import { Tag } from '@prisma/client'
-import Editor from './editor'
+import { Editor } from '@bytemd/react'
+import gfm from '@bytemd/plugin-gfm'
+import breaks from '@bytemd/plugin-breaks'
+import frontmatter from '@bytemd/plugin-frontmatter'
+import gemoji from '@bytemd/plugin-gemoji'
+import highlight from '@bytemd/plugin-highlight-ssr'
+import math from '@bytemd/plugin-math-ssr'
+import 'katex/dist/katex.css'
+import 'highlight.js/styles/default.css'
+import 'bytemd/dist/index.css'
 import TagSelector from './tag.selector'
 
 export default function PostForm() {
@@ -73,7 +82,20 @@ export default function PostForm() {
         className="placeholder:text-base md:placeholder:text-lg border-2 border-[#333333] placeholder:font-semibold font-semibold placeholder:text-[#333333] text-[#333333] w-full text-base md:text-lg rounded my-4 focus:outline-none overflow-hidden resize-none p-2"
       />
       <TagSelector tags={tags} selectedTags={tags} setSelectedTags={setTags} />
-      <Editor value={content} onChange={handleContentChange} />
+      <Editor
+        plugins={[
+          gfm(),
+          breaks(),
+          frontmatter(),
+          gemoji(),
+          highlight(),
+          math(),
+        ]}
+        key="editor"
+        onChange={handleContentChange}
+        placeholder="Escreva algo..."
+        value={content}
+      />
       <button
         type="submit"
         className="bg-[#333333] text-white rounded px-4 py-2 w-full sm:w-auto"
