c9c6f67879ae208642b8ec563a829fd31cb72612
Edilson Rogério Cuambe <edilson@aluno.unilab.edu.br>
2024-08-09 18:28:28 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import Hero from '@/components/hero'
 import LatestNews from '@/components/latest.news'
 import TechStack from '@/components/tech.stack'
-import GitHubContributions from '@/components/gitHub.contributions'
 
 export const metadata: Metadata = {
   title: {
@@ -34,7 +33,6 @@ export default function Page() {
       <Hero />
       <LatestNews />
       <TechStack />
-      <GitHubContributions />
     </main>
   )
 }
