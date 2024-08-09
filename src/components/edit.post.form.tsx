6cdae6af9892b93334cea5ec45c3f703b90a9ad0
Edilson Rogério Cuambe <edilson@aluno.unilab.edu.br>
2024-08-09 01:36:54 -0300

    remove top-0

--- src/components/edit.post.form.tsx
+++ src/components/edit.post.form.tsx
@@ -173,7 +173,7 @@ export default function EditPostForm({ post, tags }: EditPostFormProps) {
 
   return (
     <>
-      <div className="sticky top-0 z-10 mb-4 dark:bg-yellow-400 text-zinc-800 rounded-lg p-4 flex justify-between items-center">
+      <div className="sticky z-10 mb-4 dark:bg-yellow-400 text-zinc-800 rounded-lg p-4 flex justify-between items-center">
         <span className="text-lg font-semibold">
           {isPublished ? 'Arquivar' : 'Publicar'} Blog
         </span>
